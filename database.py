3867a62a7497246413586cc9d10ac9edcd54e166
gusta01010 <sonic8783@hotmail.com>
2024-07-15 16:59:36 -0300

    mudança do banco de dados para arquivos individuais

--- database.py
+++ database.py
@@ -1,12 +1,11 @@
 database = {}
-class Menu:
+class Database:
     def __init__(self):
         self.option = 0
-    def menu(self):
-        print("Bem-vindo ao Banco Central, gostaria de escolher qual opção?\n1 - Acessar conta\n2 - Criar conta\n3 - Verificar existencia de conta\n4 - Deletar conta")
         
     def criarConta(self):
-        database.update({str(input("User: ")): str(input("Pass: "))})  
+        database.update({str(input("User: ")): str(input("Pass: "))})  #Inserir no banco de dados
+        print("Usuário criado com sucesso") #Criação do usuário
         
     def verificarConta(self):
         self.acc_found = False
@@ -22,27 +21,9 @@ class Menu:
     def mostrarContas(self):
         print(database)
  
-
-class Conta:
-    def __init__(self, name, password):
-        self.username = name
-        self.password = password
-
     
     
 teste = {}
 teste.update({'skibidi': 'toilet'})
 teste.update({'toilet': 'nis'})
 
-while True:
-    Banco = Menu()
-    Banco.menu()
-    option = (int(input()))
-    if option == 1:
-        pass
-    if option == 2:
-        Banco.criarConta()
-    if option == 3:
-        Banco.verificarConta()
-    if option == 5:
-        Banco.mostrarContas()
