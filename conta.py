4fafb173df911b6c51af6b425353e0168a9bf907
gusta01010 <sonic8783@hotmail.com>
2024-07-16 17:42:22 -0300

    atualizacao para as contas atualizadas com o novo banco de dados refeito

--- conta.py
+++ conta.py
@@ -1,4 +1,25 @@
+from database import database
 class Conta:
     def __init__(self, name, password):
         self.username = name
         self.password = password
+        self.switch = False
+        for i in enumerate(database):    
+            if self.password in list(database.values())[i[0]] and self.username in list(database.keys())[i[0]]:
+                self.saldo = (list(list(database.values())[i[0]].values())[0])
+                
+                
+    def verify(self):
+        self.acc_found = False
+        for i in enumerate(database):    
+            if self.password in list(database.values())[i[0]] and self.username in list(database.keys())[i[0]]:
+                print ("User exists")
+                self.acc_found = True
+                self.switch = True
+                
+        if not self.acc_found:
+            print("Erro, verifique os dados novamente.")
+            self.switch = False
+    
+    def extrato(self):
+            print(f"Titular: {self.username} | Saldo: {self.saldo}")
