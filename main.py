0fbde32d56ec9603b1504536af25f641c0edec4e
gusta01010 <sonic8783@hotmail.com>
2024-07-18 19:07:08 -0300

    Troca de if conta.switch para while conta.switch, assim o usuário fica preso na tela de sua conta até deslogar | acréscimo de novas opções na conta como: depositar(), sacar(), sair da conta (desliga conta.switch) e opção externa de fecharConta() (Deletar a conta).

--- main.py
+++ main.py
@@ -12,19 +12,30 @@ while True:
         if option == 1:
             conta = Conta(input("User: "), input("Pass: "))
             conta.verify()
-            if conta.switch:
+            option = 0
+            while conta.switch: #Enquanto a estiver logado...
                 option = 0
-                print("Bem vindo a sua conta, selecione a opção desejada:\n\n1 - Extrato")
+                print("Bem vindo a sua conta, selecione a opção desejada:\n\n1 - Extrato\n2 - Depositar\n3 - Sacar\n4 - Deslogar")
                 option = int(input())
                 if option == 1:
                     conta.extrato()
+                elif option == 2:
+                    conta.depositar()
+                elif option == 3:
+                    conta.sacar()
+                elif option == 4:
+                    conta.switch = False
                     option = 0
+                
         elif option == 2:
             banco.criarConta()
             option = 0
             
         elif option == 3:
             banco.verificarConta()
+            option = 0
+        elif option == 4:
+            banco.fecharConta()
             option = 0
         elif option == 5:
             banco.mostrarContas()
